590a78793ca78dbd84ce640f2b724ae1d04c5667
mszalkowski <mac.szalkowski@gmail.com>
2021-06-10 23:29:27 +0200

    update structure

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = app => {
-    app.use(app.static(path.join('../client', 'build')));
+    app.use(app.static(path.join('./client', 'build')));
     app.get('/', function(req, res) {
-        res.sendFile(path.join('../client', 'build', 'index.html'));
+        res.sendFile(path.join('./client', 'build', 'index.html'));
     });
 };
